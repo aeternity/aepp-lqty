91f2d2aff4fbe99fb7700a0d57aed0191fbd1779
Badi Ifaoui <badi1wow@gmail.com>
2023-07-05 16:04:14 +0100

    feat(header): connected account, switch account, active network

--- src/components/Account/HeaderAccountCard.vue
+++ src/components/Account/HeaderAccountCard.vue
@@ -1,36 +1,73 @@
 <template>
-    <div v-if="accounts.activeAccount" class="d-flex align-center px-4">
-        <v-chip @click="accountDetailDialog = true">
-            <v-avatar start>
-                <v-img
-                    :src="`https://avatars.z52da5wt.xyz/${accounts.activeAccount}`"
-                />
-            </v-avatar>
-
-            {{ formatAddress(accounts.activeAccount) }}
+    <div v-if="activeAccount" class="d-flex align-center px-4">
+        <v-chip v-if="walletInfo" class="mr-4">
+            <v-icon color="blue" class="mr-2" size="14">mdi-wallet</v-icon>
+            {{ walletInfo?.name }}
         </v-chip>
+        <v-chip v-if="activeNetwork" class="mr-4">
+            <v-icon color="green" class="mr-2" size="14">mdi-circle</v-icon>
+            {{ activeNetwork.name }}
+        </v-chip>
+        <v-menu>
+            <template v-slot:activator="{ props }">
+                <v-chip v-bind="props">
+                    <v-avatar start loading>
+                        <v-progress-circular v-if="scanningForAccounts" indeterminate color="primary"/>
+                        <v-img
+                            v-else
+                            :src="`https://avatars.z52da5wt.xyz/${activeAccount}`"
+                        />
+                    </v-avatar>
 
-        <div class="pl-4" v-if="!balances.balance?.isZero">
-            {{ balances.balance.prettify(2) }} AE
+                    {{ formatAddress(activeAccount) }}
+                </v-chip>
+            </template>
+            <v-list>
+                <template v-for="address in accounts" :key="address">
+                    <v-list-item
+                        v-if="activeAccount !== address"
+                        :value="address"
+                        @click="setActiveAccount(address)"
+                    >
+                        <template v-slot:prepend>
+                            <v-avatar start size="24">
+                                <v-img
+                                    :src="`https://avatars.z52da5wt.xyz/${address}`"
+                                />
+                            </v-avatar>
+                        </template>
+                        <v-list-item-title>
+                            {{ formatAddress(address) }}
+                        </v-list-item-title>
+                    </v-list-item>
+                </template>
+                <v-list-item @click="disconnectWallet()">
+                    <v-list-item-title class="text-red">
+                        Disconnect
+                    </v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+
+        <div class="pl-4">
+            {{ !balance?.isZero ? balance.prettify(2) : 0 }} AE
         </div>
 
         <v-dialog v-model="accountDetailDialog" max-width="600px">
             <v-card>
                 <v-card-title>
-                    {{ formatAddress(accounts.activeAccount) }}
+                    {{ formatAddress(activeAccount) }}
                 </v-card-title>
                 <v-card-text>
-                    <div>
-                        <strong>Wallet:</strong> {{ wallet.walletInfo?.name }}
-                    </div>
+                    <div><strong>Wallet:</strong> {{ walletInfo?.name }}</div>
 
                     <div>
                         <strong>Network ID:</strong>
-                        {{ wallet.walletInfo?.networkId }}
+                        {{ walletInfo?.networkId }}
                     </div>
                 </v-card-text>
                 <v-card-actions>
-                    <v-btn color="red" @click="wallet.disconnectWallet()">
+                    <v-btn color="red" @click="disconnectWallet()">
                         Disconnect
                     </v-btn>
                 </v-card-actions>
@@ -42,27 +79,43 @@
 <script lang="ts">
 import { useAccounts } from "@/store/accounts";
 import { useBalances } from "@/store/balances";
+import { useNetworks } from "@/store/networks";
 import { useWalletConnect } from "@/store/walletConnect";
 import { formatAddress } from "@/utils";
+import { storeToRefs } from "pinia";
 import { ref } from "vue";
 
 export default {
     name: "HeaderAccountCard",
     setup() {
-        const accounts = useAccounts();
-        const balances = useBalances();
-        const wallet = useWalletConnect();
+        const { activeNetwork } = storeToRefs(useNetworks());
+        const { setActiveAccount } = useAccounts();
+        const { activeAccount, accounts } = storeToRefs(useAccounts());
+
+        const { balance } = storeToRefs(useBalances());
+
+        const { walletInfo, scanningForAccounts } = storeToRefs(
+            useWalletConnect()
+        );
+        const { disconnectWallet } = useWalletConnect();
 
         const accountDetailDialog = ref(false);
 
         return {
-            balances,
-            wallet,
-            accounts,
+            scanningForAccounts,
 
+            balance,
+            activeAccount,
+            accounts,
+            setActiveAccount,
             accountDetailDialog,
 
             formatAddress,
+
+            walletInfo,
+            disconnectWallet,
+
+            activeNetwork,
         };
     },
 };
