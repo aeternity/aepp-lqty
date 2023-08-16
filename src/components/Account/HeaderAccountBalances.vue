ca210b5e9decd1ec571b458d2e63b88aaa68c2a9
Badi Ifaoui <badi1wow@gmail.com>
2023-08-16 16:28:54 +0100

    feat: show ae, aeusd, lqty balances

---
+++ src/components/Account/HeaderAccountBalances.vue
@@ -0,0 +1,27 @@
+
+<template>
+    <div class="d-flex">
+        <div
+            v-for="(key, index) of Object.keys(balances)"
+            :key="index"
+            class="text-center px-2"
+        >
+            <strong>{{ key }}</strong>
+            <div>{{ balances[key].shorten() }}</div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { useBalances } from "@/store/balances";
+import { storeToRefs } from "pinia";
+export default {
+    setup() {
+        const { balances } = storeToRefs(useBalances());
+
+        return {
+            balances,
+        };
+    },
+};
+</script>
