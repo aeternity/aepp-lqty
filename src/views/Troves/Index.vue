f71256d5163dba9752917e1689d97ec51278df17
Badi Ifaoui <badi1wow@gmail.com>
2023-08-16 12:59:46 +0100

    feat: the ability to liquidate risky troves

--- src/views/Troves/Index.vue
+++ src/views/Troves/Index.vue
@@ -12,35 +12,55 @@
             text="In the initial stages, you might have the option to manually liquidate Troves; however, as the system evolves, this capability is likely to diminish."
         ></v-alert>
 
-        <v-data-table
-            :loading="loading"
-            v-model:items-per-page="itemsPerPage"
-            :headers="headers"
-            :items="items"
-        >
-            <template v-slot:[`item.owner`]="{ item: { selectable } }">
-                <v-chip pill link>
-                    <v-avatar size="30" class="mr-2" start>
-                        <v-img
-                            :src="`https://avatars.z52da5wt.xyz/${selectable.owner}`"
-                        />
-                    </v-avatar>
-                    {{ formatAddress(selectable.owner) }}
-                </v-chip>
-            </template>
+        <v-card>
+            <v-card-title class="d-flex align-center">
+                <h3 class="text-h5">Open Troves</h3>
+                <v-spacer></v-spacer>
+                <v-btn flat icon @click="fetchOpenTroves()">
+                    <v-icon>mdi-reload</v-icon>
+                </v-btn>
+            </v-card-title>
+            <v-data-table
+                flat
+                :loading="loading"
+                v-model:items-per-page="itemsPerPage"
+                :headers="headers"
+                :items="items"
+            >
+                <template v-slot:[`item.owner`]="{ item: { selectable } }">
+                    <v-chip pill link>
+                        <v-avatar size="30" class="mr-2" start>
+                            <v-img
+                                :src="`https://avatars.z52da5wt.xyz/${selectable.owner}`"
+                            />
+                        </v-avatar>
+                        {{ formatAddress(selectable.owner) }}
+                    </v-chip>
+                </template>
 
-            <template v-slot:[`item.collateral`]="{ item: { selectable } }">
-                {{ selectable.trove.collateral.prettify(2) }}
-            </template>
+                <template v-slot:[`item.collateral`]="{ item: { selectable } }">
+                    {{ selectable.trove.collateral.prettify(2) }}
+                </template>
 
-            <template v-slot:[`item.debt`]="{ item: { selectable } }">
-                {{ selectable.trove.debt.prettify(2) }}
-            </template>
+                <template v-slot:[`item.debt`]="{ item: { selectable } }">
+                    {{ selectable.trove.debt.prettify(2) }}
+                </template>
 
-            <template v-slot:[`item.coll_ratio`]="{ item: { selectable } }">
-                <CollateralRatioValue :trove="selectable.trove" />
-            </template>
-        </v-data-table>
+                <template v-slot:[`item.coll_ratio`]="{ item: { selectable } }">
+                    <CollateralRatioValue :trove="selectable.trove" />
+                </template>
+
+                <template v-slot:[`item.action`]="{ item: { selectable } }">
+                    <v-btn
+                        color="red"
+                        :loading="loading"
+                        @click="onLiquidateTrove(selectable.owner)"
+                    >
+                        Liquidate
+                    </v-btn>
+                </template>
+            </v-data-table>
+        </v-card>
     </div>
 </template>
 
@@ -51,6 +71,7 @@ import { useAeppSdk } from "@/composables";
 import { formatAddress } from "@/utils";
 import { Decimal, Percent, Trove } from "@liquity/lib-base";
 import { onMounted, ref } from "vue";
+import { useLiquityStore } from "@/store/liquityStore";
 
 export default {
     components: {
@@ -59,6 +80,7 @@ export default {
     },
     setup() {
         const { contracts } = useAeppSdk();
+        const { preloadInitialData } = useLiquityStore();
 
         const loading = ref(false);
         const headers = [
@@ -71,11 +93,30 @@ export default {
             { title: "Collateral", align: "end", key: "collateral" },
             { title: "Debt", align: "end", key: "debt" },
             { title: "Coll. Ratio", align: "end", key: "coll_ratio" },
+            { title: "", align: "end", key: "action" },
         ];
         const itemsPerPage = ref(5);
         const items = ref<any[]>([]);
 
+        async function onLiquidateTrove(address: string) {
+            loading.value = true;
+            try {
+                await contracts.TroveManager.methods.liquidate(address);
+                // remove trove from list
+                items.value = items.value.filter(
+                    (item) => item.owner !== address
+                );
+                preloadInitialData();
+            } catch (error) {
+                console.info("========================");
+                console.info("onLiquidateTrove->error ::", error);
+                console.info("========================");
+            }
+            loading.value = false;
+        }
+
         async function fetchOpenTroves() {
+            items.value = [];
             loading.value = true;
             const openTrovesCount = (
                 await contracts.TroveManager.methods.get_trove_owners_count()
@@ -118,6 +159,8 @@ export default {
 
         return {
             loading,
+            onLiquidateTrove,
+            fetchOpenTroves,
 
             formatAddress,
 
