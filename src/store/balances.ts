23a78c2c0f30e53b607c51944c4672ce907ae2c0
Badi Ifaoui <badi1wow@gmail.com>
2023-08-16 16:14:32 +0100

    feat: improved balances loader

--- src/store/balances.ts
+++ src/store/balances.ts
@@ -1,47 +1,86 @@
-// Utilities
 import { useAeppSdk } from "@/composables";
+import { AEUSD_SYMBOL, AE_SYMBOL, LQTY_SYMBOL } from "@/utils";
 import { Decimal } from "@liquity/lib-base";
-import { defineStore } from "pinia";
+import { defineStore, storeToRefs } from "pinia";
 import { computed, onMounted, ref, watch } from "vue";
 import { useAccounts } from "./accounts";
 
 export const useBalances = defineStore(
   "balances",
   () => {
-    const { getSdk } = useAeppSdk();
-    const accounts = useAccounts();
-    const balances = ref<Record<string, string>>({});
-    const balance = computed<Decimal>(() =>
-      Decimal.fromBigNumberString(balances.value[accounts.activeAccount] ?? 0)
+    const { getSdk, contracts } = useAeppSdk();
+    const { activeAccount } = storeToRefs(useAccounts());
+
+    const _balances = ref<Record<string, Record<string, string>>>({});
+
+    const balances = computed<Record<string, Decimal>>(() => {
+      const newBalances: Record<string, Decimal> = {};
+      if (activeAccount.value && _balances.value[activeAccount.value]) {
+        Object.keys(_balances.value[activeAccount.value]).forEach((key) => {
+          if (
+            activeAccount.value &&
+            _balances.value[activeAccount.value][key]
+          ) {
+            newBalances[key] = Decimal.fromBigNumberString(
+              _balances.value[activeAccount.value][key]
+            );
+          }
+        });
+      }
+      return newBalances;
+    });
+
+    // AE balance
+    const balance = computed<Decimal>(
+      () => balances.value[AE_SYMBOL] || Decimal.ZERO
     );
 
-    async function loadBalance() {
+    const aeusdBalance = computed<Decimal>(
+      () => balances.value[AEUSD_SYMBOL] || Decimal.ZERO
+    );
+
+    const lqtyBalance = computed<Decimal>(
+      () => balances.value[LQTY_SYMBOL] || Decimal.ZERO
+    );
+
+    async function loadBalances() {
+      if (!activeAccount.value) return;
+
       const aeSdk = await getSdk();
-      balances.value[accounts.activeAccount] = await aeSdk.getBalance(
-        accounts.activeAccount
-      );
+
+      const [ae_balance, lqty_balance, aeusd_balance] = await Promise.all([
+        aeSdk.getBalance(activeAccount.value as any),
+        contracts.LQTYToken.methods
+          .balance(activeAccount.value)
+          .then(({ decodedResult }: any) => decodedResult),
+        contracts.AEUSDToken.methods
+          .balance(activeAccount.value)
+          .then(({ decodedResult }: any) => decodedResult),
+      ]);
+
+      _balances.value[activeAccount.value] = {
+        [AE_SYMBOL]: ae_balance,
+        [LQTY_SYMBOL]: lqty_balance,
+        [AEUSD_SYMBOL]: aeusd_balance,
+      };
     }
 
-    watch(
-      () => accounts.activeAccount,
-      (address: string, oldAddress: string) => {
-        if (address !== oldAddress) {
-          balances.value[address] = "0";
-        }
-        loadBalance();
-      }
-    );
+    watch(activeAccount, () => loadBalances());
 
+    // TODO: should be called 1 time only
     onMounted(() => {
-      loadBalance();
+      loadBalances();
 
       setInterval(() => {
-        loadBalance();
+        loadBalances();
       }, 5000);
     });
 
     return {
       balance,
+      aeusdBalance,
+      lqtyBalance,
+      balances,
     };
   },
   { persist: true }
